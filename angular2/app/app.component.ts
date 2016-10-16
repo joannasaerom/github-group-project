86ed6231eb4af7754d2661c1d8e8f3675ec7540f
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-10-15 23:33:04 -0700

    Adds bower

--- angular2/app/app.component.ts
+++ angular2/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1>My First Angular 2 App</h1>
+  </div>
   `
 })
 
 export class AppComponent{
-  
+
 }
